611120f24b40c2a7eb2d8cf74589c4cc352a6a5c
Leolasland <hdanyel@student.21-school.ru>
2023-06-11 21:14:55 +0300

    test hibernate one to one

--- src/main/java/ru/swiftbook/App.java
+++ src/main/java/ru/swiftbook/App.java
@@ -5,6 +5,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.swiftbook.model.Movie;
 import ru.swiftbook.model.Director;
+import ru.swiftbook.model.Principal;
+import ru.swiftbook.model.School;
 
 import java.util.List;
 
@@ -14,8 +16,8 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Director.class)
-                .addAnnotatedClass(Movie.class);
+                .addAnnotatedClass(Principal.class)
+                .addAnnotatedClass(School.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -23,44 +25,23 @@ public class App {
         try {
             session.beginTransaction();
 
-//            Director director = session.get(Director.class, 1);
-//            System.out.println(director);
-//            List<Movie> movies = director.getMovies();
-//            System.out.println(movies);
+//            Principal principal = session.get(Principal.class, 1);
+//            System.out.println(principal.getSchool());
 
-//            Movie movie = session.get(Movie.class, 1);
-//            System.out.println(movie);
-//            Director director = movie.getDirector();
-//            System.out.println(director);
+//            Principal principal = new Principal("Test", 42);
+//            School school = new School(1111, principal);
+//            principal.setSchool(school);
+//            session.save(principal);
 
-//            Director director = session.get(Director.class, 5);
-//            System.out.println(director.getMovies());
-//            Movie newMovie = new Movie("test movie", 2020, director);
-//            director.getMovies().add(newMovie);
-//            session.save(newMovie);
-//            System.out.println(director.getMovies());
+//            Principal principal = new Principal("Test change", 42);
+//            School school = session.get(School.class, 5);
+//            school.setPrincipal(principal);
+//            principal.setSchool(school);
+//            session.save(principal);
 
-//            Director newDirector = new Director("test director", 20);
-//            Movie newMovie = new Movie("test hibernate", 2020, newDirector);
-//            newDirector.setMovies(List.of(newMovie));
-//            session.save(newDirector);
-//            session.save(newMovie);
-//            System.out.println(newDirector);
-
-//            Movie movie = session.get(Movie.class, 12);
-//            Director director = session.get(Director.class, 7);
-//            movie.getDirector().getMovies().remove(movie);
-//            movie.setDirector(director);
-//            director.getMovies().add(movie);
-//            System.out.println(movie.getDirector());
-//            System.out.println(director.getMovies());
-
-            Director director = session.get(Director.class, 7);
-            System.out.println(director.getMovies());
-            Movie movie = session.get(Movie.class, 13);
-            session.remove(movie);
-            director.getMovies().remove(movie);
-            System.out.println(director.getMovies());
+//            School school = session.get(School.class, 6);
+//            Principal principal = session.get(Principal.class, 1);
+//            school.setPrincipal(principal);
 
             session.getTransaction().commit();
         } finally {
@@ -69,4 +50,45 @@ public class App {
             sessionFactory.close();
         }
     }
+
+//    public void firstTask(Session session) {
+//        Director director = session.get(Director.class, 1);
+//        System.out.println(director);
+//        List<Movie> movies = director.getMovies();
+//        System.out.println(movies);
+//
+//        Movie movie = session.get(Movie.class, 1);
+//        System.out.println(movie);
+//        Director director = movie.getDirector();
+//        System.out.println(director);
+//
+//        Director director = session.get(Director.class, 5);
+//        System.out.println(director.getMovies());
+//        Movie newMovie = new Movie("test movie", 2020, director);
+//        director.getMovies().add(newMovie);
+//        session.save(newMovie);
+//        System.out.println(director.getMovies());
+//
+//        Director newDirector = new Director("test director", 20);
+//        Movie newMovie = new Movie("test hibernate", 2020, newDirector);
+//        newDirector.setMovies(List.of(newMovie));
+//        session.save(newDirector);
+//        session.save(newMovie);
+//        System.out.println(newDirector);
+//
+//        Movie movie = session.get(Movie.class, 12);
+//        Director director = session.get(Director.class, 7);
+//        movie.getDirector().getMovies().remove(movie);
+//        movie.setDirector(director);
+//        director.getMovies().add(movie);
+//        System.out.println(movie.getDirector());
+//        System.out.println(director.getMovies());
+//
+//        Director director = session.get(Director.class, 7);
+//        System.out.println(director.getMovies());
+//        Movie movie = session.get(Movie.class, 13);
+//        session.remove(movie);
+//        director.getMovies().remove(movie);
+//        System.out.println(director.getMovies());
+//    }
 }
